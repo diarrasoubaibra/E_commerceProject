ac27e4f0fcc709f7a5031b2e8fa74c4d20640f49
Diarrassouba ibrahim <diarrassoubaibra58@gmail.com>
2023-05-10 12:02:32 +0000

    first-step

---
+++ store/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=128)
+    slug = models.SlugField(max_length=128)
+    price = models.FloatField(default=0.0)
+    stock = models.IntegerField(default=0)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to="products", null=True)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse("product", kwargs={"slug":self.slug})
